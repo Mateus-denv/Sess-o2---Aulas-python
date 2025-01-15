072f6870b26cac5201ec6a15a613492cc5d584ce
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2025-01-14 22:54:42 -0300

    Introdução a Dicionários
    Criado: Aula
    Conteudo: Dicionários

--- aula119a123.py
+++ aula119a123.py
@@ -2,19 +2,29 @@ pessoa = {
 
 }
 
-pessoa['nome'] = 'Matheus'
+pessoa['Nome'] = 'Matheus'
 pessoa['Codigo'] = [
     {'COD1': '123'},
     {'COD2': '456'},
 ]
 
-print(pessoa['Codigo'][0]['COD1'])
+#print(pessoa['Codigo'][0]['COD1'])
 
-del pessoa['Codigo'][0]['COD1']
+#del pessoa['Codigo'][0]['COD1']
 
-print(pessoa)
+#print(pessoa)
 
 if  pessoa.get('Codigo') is None:
     print('NÃO EXISTE')
 else:
     print('EXISTE')
+
+pessoa.setdefault('Idade', 0)
+# print(pessoa.__len__())
+# print(len(pessoa))
+# print(list(pessoa.keys()))
+# print(list(pessoa.values()))
+print(list(pessoa.items()))
+
+for chave in pessoa.values():
+    ...#print(chave)
