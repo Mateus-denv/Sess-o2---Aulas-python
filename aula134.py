cc36255651692faaefdef74388d5bdc7c332f085
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2025-02-02 15:22:50 -0300

    Funções lambda complexas

--- aula134.py
+++ aula134.py
@@ -1,22 +1,27 @@
 def executa(funcao, *args):
     return funcao(*args)
 
-def soma(x, y):
+"""def soma(x, y):
     return x + y
 
 def criar_multiplicador(multiplicador):
     def multiplica(numero):
-        return multiplica
-    
-print(
-    executa(
-        lambda x, y: x + y, 2,3
-    ),
-    executa(soma(2, 3)),
-    soma(2,3)
+        return multiplica"""
+um = 1
+dois = 2
+
+duplica = executa(
+    lambda m: lambda n: n * m, dois
 )
 
-duplica = criar_multiplicador(2)
-duplica = executa(
-    lambda m: lambda n: n * m, 2
+print(duplica(2))
+
+print(
+    executa(
+        lambda x, y: x + y, 
+        um,dois
+    ),
 )
+
+# duplica = criar_multiplicador(2)
+
