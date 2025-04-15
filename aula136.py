97f971c2bfb1ccbf19e4ac6ba2c881773ab161d1
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2025-04-14 22:10:53 -0300

    Update aula136.py

--- aula136.py
+++ aula136.py
@@ -7,20 +7,21 @@ for numero in range(20):
     
 lista = [numero * 2 for numero in range (10)] # Adiciona um valor na lista e multiplica por um valor deseijado
 
-print(lista)
-print(list(range(10)))
+"""print(lista)
+print(list(range(10)))"""
 
 # Mapeamento de dados em list comprehension
 produtos = [
     {'nome': 'p1','Valor': 20,},
     {'nome': 'p2','Valor': 10,},
     {'nome': 'p3','Valor': 56,},
-    {'nome': 'p4','Valor': 18,},
+    {'nome': 'p4','Valor': 30,},
 ]
 novos_produtos = [
-    {**produto, 'preco': produto['preco'] * 1.05}
-    if produto['preco'] > 20 else {**produto}
+    {**produto, 'Valor': produto['Valor'] * 1.05}
+    if produto['Valor'] > 20 else {**produto}
     for produto in produtos
+    if produto['Valor'] >= 11
 ]
 
 # print(novos_produtos)
