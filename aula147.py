a9c2167dcdb403b0046a8a8bd6c08b7b44a0e350
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2025-05-20 20:07:03 -0300

    Yield from

--- aula147.py
+++ aula147.py
@@ -106,3 +106,18 @@ Use quando:
 | Consome mais memória    | Usa memória sob demanda |
 
 """
+# Yield from
+
+def gen1():
+    yield 1
+    yield 2
+    yield 3
+def gen2():
+    yield from gen1()
+    yield 4
+    yield 5
+    yield 6
+    
+g = gen2()
+for numero in g:
+    print(numero)
