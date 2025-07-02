0cb5e141db9d71e4ea59c213c7d748c73e48b7fd
Mateus-denv <mateussant0sc0osta@outlook.com>
2025-07-02 19:53:36 -0300

    Operadores aritiméticos
     Ordem de execução

--- Relembrando assuntos python.py
+++ Relembrando assuntos python.py
@@ -13,3 +13,11 @@ print(produto1 / produto2)
 print(produto1 // produto2)
 print(produto1 % produto2)
 print(produto1 ** produto2)
+
+### Ordem de execução
+print("\nOrdem de execução")
+
+#        10    +    20    -    10    *    20
+print(produto1 + produto2 - produto1 * produto2)
+#        10    +    20    -    10    *    20
+print(produto1 + produto2 - ((produto1 * produto2) // produto1 % produto2))
