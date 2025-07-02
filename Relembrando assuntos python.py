3138d179f6a2bf307e673ef8cf0730de272185c9
Mateus-denv <mateussant0sc0osta@outlook.com>
2025-07-02 19:53:11 -0300

    Operadores aritméticos

--- Relembrando assuntos python.py
+++ Relembrando assuntos python.py
@@ -1,2 +1,15 @@
 # Relembrando assuntos python
 ## Tipos de operadoes
+### Operadores aritméticos
+print("Operadores aritméticos")
+
+produto1 = 10
+produto2 = 20
+
+print(produto1 + produto2)
+print(produto1 - produto2)
+print(produto1 * produto2)
+print(produto1 / produto2)
+print(produto1 // produto2)
+print(produto1 % produto2)
+print(produto1 ** produto2)
