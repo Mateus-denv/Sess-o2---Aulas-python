0572d620caa90f596059ea2957857c93839599d9
Mateus-denv <mateussant0sc0osta@outlook.com>
2025-07-04 18:56:17 -0300

    Correções de sintaxe
    Correções de sintaxe

--- desafio1.py
+++ desafio1.py
@@ -11,24 +11,27 @@ limite_para_saque = 500
 extrato = {}
 numero_de_saques = 0
 LIMITES_DE_SAQUES = 3
-       
+cont = 0       
+
         
 while True:
     print("BANCO PAN")
-    cont = 0
+    
     opcao = input(menu).upper()
     
     if opcao == "D":# Depositar
-        entrada = int(input("Qual o valor do deposito:\n>>>"))
+        entrada = float(input("Qual o valor do deposito:\n>>>"))
+        
         if entrada > 0:
             cont += 1
             saldo += entrada
-            extrato.update({f"Entrada{cont}":entrada})
+            extrato.update({f"Deposito{cont}": f"R$ {entrada}"})
         else:
             print("Não foi possivel realizar o deposito")
                 
     elif opcao == "S":# Saldo
-        print(f"Seu saldo atual é de {saldo}")
+        print(f"Seu saldo atual é de R$ {saldo}")
+        
     elif opcao == "C":# Sacar
         if LIMITES_DE_SAQUES != 0:
             sacar = float(input(f"Quanto deseja sacar?\n>>>"))
