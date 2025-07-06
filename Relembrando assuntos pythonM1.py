9a44875dd4a5b657660fa1501a93e8d7b30be75e
Mateus-denv <mateussant0sc0osta@outlook.com>
2025-07-06 17:38:51 -0300

    Args e Kwargs

--- Relembrando assuntos pythonM1.py
+++ Relembrando assuntos pythonM1.py
@@ -261,3 +261,22 @@ print("\nNOMEADO COMO DICIONARIO")
 salva_moto(**{"marca": "HONDA", "modelo":"CG125","ano": 2015, "quilometragem" : 1200.2})
 """
 
+# *args trabalham com lista e tuplas
+# **Kwargs trabalham com discionarios ou chave e valor
+
+def exibir_poema(data_extenso, *args, **kwargs):
+    texto = "\n".join(args)
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    print(mensagem)
+    
+exibir_poema("Zen of python", "Beautigful is better than ugly",
+             "Beautigful is better than ugly",
+             "Beautigful is better than ugly",
+             "Beautigful is better than ugly",
+             "Beautigful is better than ugly",
+             "Beautigful is better than ugly",
+             "Beautigful is better than ugly",
+             "Beautigful is better than ugly",
+             "Beautigful is better than ugly",
+             autor="Tim Peters", ano=1999)
