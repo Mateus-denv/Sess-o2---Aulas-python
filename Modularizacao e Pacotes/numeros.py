007320aa02d86798e635c1f463ebfb16bcd30946
Mateus-denv <mateussant0sc0osta@outlook.com>
2025-07-18 10:23:12 -0300

    Create numeros.py

---
+++ Modularizacao e Pacotes/numeros.py
@@ -0,0 +1,7 @@
+import utils.calculos
+
+numero =  int(input("Digiteo o numero: "))
+
+print(utils.calculos.fatorial(numero))
+print(utils.calculos.dobro(numero))
+print(utils.calculos.triplo(numero))
